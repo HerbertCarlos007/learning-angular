22c17aeca6f61b1283e27ab2b1442611fdc529d7
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2023-09-17 20:18:38 -0300

    emitindo eventos do componente filho para o pai

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,9 @@ import { FirstComponentComponent } from './components/first-component/first-comp
 import { ParentDataComponent } from './components/parent-data/parent-data.component';
 import { DirectivesComponent } from './components/directives/directives.component';
 import { IfRenderComponent } from './components/if-render/if-render.component';
-import { EventosComponent } from './components/eventos/eventos.component'
+import { EventosComponent } from './components/eventos/eventos.component';
+import { EmitterComponent } from './components/emitter/emitter.component';
+import { ChangeNumberComponent } from './components/change-number/change-number.component'
 
 
 @NgModule({
@@ -18,6 +20,8 @@ import { EventosComponent } from './components/eventos/eventos.component'
     DirectivesComponent,
     IfRenderComponent,
     EventosComponent,
+    EmitterComponent,
+    ChangeNumberComponent,
   ],
   imports: [
     BrowserModule,
